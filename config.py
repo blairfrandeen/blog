7233bb536fa950c6cd5659302df14079e10a723d
Blair Frandeen <blairfrandeen@gmail.com>
2023-02-04 07:29:03 -0800

    fix: update secret key

--- config.py
+++ config.py
@@ -4,11 +4,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
-    # TODO: Make this an actual secret
-    SECRET_KEY = (
-        os.environ.get("SECRET_KEY")
-        or "6b020b802d2a6b4128635201c40e59fea21440b979da3097286edc39fb0d6c04"
-    )
+    SECRET_KEY = os.environ.get("SECRET_KEY") or "go-look-in-your-password-manaager"
     SQLALCHEMY_DATABASE_URI = os.environ.get(
         "DATABASE_URL"
     ) or "sqlite:///" + os.path.join(basedir, "blog.db")
