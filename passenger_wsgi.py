288c74a8a24ea797c750114c779020bb6e3e85d1
Blair Frandeen <blairfrandeen@gmail.com>
2023-02-04 07:53:38 -0800

    fix: remove python f-string

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,6 +1,6 @@
 import sys, os
 
-INTERP = f"{os.path.expanduser('~')}/datum-b.com/venv/bin/python3"
+INTERP = os.path.expanduser("~") + "/datum-b.com/venv/bin/python3"
 # INTERP is present twice so that the new Python interpreter knows the actual executable path
 if sys.executable != INTERP:
     os.execl(INTERP, INTERP, *sys.argv)
