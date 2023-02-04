9e53087cdfde9691742b86575624ed9e9eb3eb44
Blair Frandeen <blairfrandeen@gmail.com>
2023-02-04 07:35:16 -0800

    fix: remove username from passenger_wsgi.py

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,6 +1,6 @@
 import sys, os
 
-INTERP = "/home/***REMOVED***/datum-b.com/venv/bin/python3"
+INTERP = f"{os.path.expanduser('~')}/datum-b.com/venv/bin/python3"
 # INTERP is present twice so that the new Python interpreter knows the actual executable path
 if sys.executable != INTERP:
     os.execl(INTERP, INTERP, *sys.argv)
